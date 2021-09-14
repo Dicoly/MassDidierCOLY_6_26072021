cfe4a437e16d7002a8ea98c8a4694b65c162d0f7
Mass Didier Coly <mass-didier.coly@expleogroup.com>
2021-09-14 15:20:32 +0200

    Add test BillsUI 100% views

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -19,4 +19,19 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
   })
+  // Pour les 100% views ajout test BillsUI loadingPage et errorPage
+  describe('When I am on Bills page but it is loading', () => {
+    test('Then, Loading page should be rendered', () => {
+      const html = BillsUI({ loading: true })
+      document.body.innerHTML = html
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
+    })
+  })
+  describe('When I am on Bills page but back-end send an error message', () => {
+    test('Then, Error page should be rendered', () => {
+      const html = BillsUI({ error: 'some error message' })
+      document.body.innerHTML = html
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
 })
