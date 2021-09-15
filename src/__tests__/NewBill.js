15723ce4ab0225888f64f269b9f1963d480fcb2d
Mass Didier Coly <mass-didier.coly@expleogroup.com>
2021-09-15 14:34:06 +0200

    Add test integration POST NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,6 +3,8 @@ import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import userEvent from "@testing-library/user-event"
 import { ROUTES} from "../constants/routes.js"
+import BillsUI from "../views/BillsUI"
+import firebase from "../__mocks__/firebase"
 
 
 import {setSessionStorage} from "../../setup-jest";
@@ -164,4 +166,48 @@ describe("Given I am connected as an employee", () => {
 			expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
     });
   });
+})
+// test d'intégration POST
+describe("Given I am a user connected as an Employee", () => {
+  describe("When I create a new bill", () => {
+    test("it should add bill to mock API POST", async () => {
+       const postSpy = jest.spyOn(firebase, "post")
+       const newBill = {
+        "id": "cqDH5SzMAecZAGRrHjaC",
+        "vat": "80",
+        "fileUrl": "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        "status": "pending",
+        "type": "Hôtel et logement",
+        "commentary": "Vacance",
+        "name": "Voyage Londre",
+        "fileName": "preview-facture-pdf-1.jpg",
+        "date": "2021-09-10",
+        "amount": 345,
+        "commentAdmin": "ok",
+        "email": "paco@mail.com",
+        "pct": 20
+       }
+       const bills = await firebase.post(newBill)
+       expect(postSpy).toHaveBeenCalledTimes(1)
+       expect(bills.data.length).toBe(5)
+    })
+    test("it should add bill from an API but fails with 404 message error", async () => {
+      firebase.post.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 404"))
+      )
+      const html = BillsUI({ error: "Erreur 404" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("It should add bill from an API and fails with 500 message error", async () => {
+      firebase.post.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 500"))
+      )
+      const html = BillsUI({ error: "Erreur 500" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
 })
